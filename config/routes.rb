ba3d21d4a08c162d847d3f564dd9b4f87097493b
Payton Carter <paytoncarter14@gmail.com>
2012-12-19 10:59:50 -0700

    Update everything

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Family::Application.routes.draw do
+  get "static_pages/index"
+
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
@@ -48,7 +50,7 @@ Family::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-  # root :to => 'welcome#index'
+  root :to => 'static_pages#index'
 
   # See how all your routes lay out with "rake routes"
 
