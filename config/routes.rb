0a2c9b725a5792b6228546a24d5f7d6fb3ce0ec9
Payton Carter <paytoncarter14@gmail.com>
2012-12-19 14:35:34 -0700

    Added article page

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Family::Application.routes.draw do
   get "static_pages/index"
+  get "static_pages/article"
 
   # The priority is based upon order of creation:
   # first created -> highest priority.
