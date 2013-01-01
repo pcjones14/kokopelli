5255624942e3a8df2c8a883f63175fcf1ac1dabf
Payton Carter <paytoncarter14@gmail.com>
2012-12-31 17:05:20 -0700

    something

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -10,15 +10,24 @@ class ArticlesController < ApplicationController
   def new
   end
 
-  def edit
+  def create
+    Article.create(title: params[:title], body: params[:body], tags: params[:tags], visible: params[:visible])
+    redirect_to articles_path
   end
 
-  def create
+  def edit
+    @article = Article.find(params[:id])
   end
 
   def update
+    @article = Article.find(params[:id])
+    @article.update_attributes(params[:article])
+    redirect_to articles_path
   end
 
   def destroy
+    Article.destroy(params[:id])
+    redirect_to articles_path
   end
+
 end
