2eeb276e2df04c902603d2b43809b7c5c14d4c34
Payton Carter <paytoncarter14@gmail.com>
2013-12-04 17:27:04 -0700

    screw it

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -9,8 +9,7 @@
 //
 // WARNING: THE FIRST BLANK LINE MARKS THE END OF WHAT'S TO BE PROCESSED, ANY BLANK LINE SHOULD
 // GO AFTER THE REQUIRES BELOW.
-//
-//= require bootstrap
 //= require jquery
+//= require bootstrap
 //= require jquery_ujs
 //= require_tree .
