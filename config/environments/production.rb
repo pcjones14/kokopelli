bc5eec5da87ff9c74f1c2a7be9bb5c2d10be056f
Payton Carter <paytoncarter14@gmail.com>
2013-12-19 12:21:41 -0700

    shut up smtp nobody loves you

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -68,15 +68,13 @@ Family::Application.configure do
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.perform_deliveries = true
   config.action_mailer.raise_delivery_errors = true
-
-  config.action_mailer.smtp_settings = {   
-    :openssl_verify_mode => OpenSSL::SSL::VERIFY_NONE,      
-    :ssl => true,
-    :enable_starttls_auto => true,  #this is the important stuff!
-    :address        => 'smtp.mailgun.org',
-    :port           => 587,
-    :authentication => :plain,
-    :user_name      => 'postmaster@kokosurance.com',
-    :password       => '1zi4z8qhuk55'
+  config.action_mailer.smtp_settings = {
+     :authentication => :plain,
+     :address => "smtp.mailgun.org",
+     :port => 587,
+     :domain => "kokosurance.com",
+     :user_name => "postmaster@kokosurance.com",
+     :password => "1zi4z8qhuk55"
   }
+
 end
